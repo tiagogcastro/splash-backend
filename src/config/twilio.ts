45316a40c78f49351baa1fa82576af9a657db680
FireShark688 <gugacalb@gmail.com>
2021-05-28 20:08:19 -0300

    feat: Added system for sending and verifying the verification code.

---
+++ src/config/twilio.ts
@@ -0,0 +1,7 @@
+export default {
+  twilio: {
+    accountSid: 'AC110285eb414fefac458c659ce5e870c2',
+    authToken: '048254d5297419a792172391d872d10d',
+    servicesSid: 'VA759b607d79ca751b05be5b2e3d04c07e',
+  },
+};
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 
 import usersRoutes from './users.routes';
+import smsRoutes from './sms.routes';
 
 const router = Router();
 
 router.use(express.json());
 router.use('/users', usersRoutes);
+router.use('/sms', smsRoutes);
 
 export default router;
