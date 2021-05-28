34262da51a0f73cfcac51ef0fe75d3ce7e30fe0d
Tiaguin061 <tiaguin180@gmail.com>
2021-05-28 16:55:54 -0300

    feat: Using sponsor router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 
+import sponsoringRoutes from './sponsoring.routes';
 import usersRoutes from './users.routes';
 
 const router = Router();
 
 router.use(express.json());
 router.use('/users', usersRoutes);
+router.use('/sponsor', sponsoringRoutes);
 
 export default router;
