6d37fd530b7d8cfcf5221dbddfd3ca7f6a5db228
Tiaguin061 <tiaguin180@gmail.com>
2021-05-29 16:12:27 -0300

    feat: Added sponsored and sponsoring router

--- src/shared/infra/http/routes/sponsoring.routes.ts
+++ src/shared/infra/http/routes/sponsoring.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import ListSponsorController from '../controllers/ListSponsorController';
 import SponsoringController from '../controllers/SponsoringController';
 
 const sponsoringRoutes = Router();
 
 const sponsoringController = new SponsoringController();
+const listSponsorController = new ListSponsorController();
 
 sponsoringRoutes.put('/', sponsoringController.update);
 sponsoringRoutes.delete('/', sponsoringController.delete);
+sponsoringRoutes.get('/sponsoring/:user_id', listSponsorController.indexSponsoring);
+sponsoringRoutes.get('/sponsored/:user_id', listSponsorController.indexSponsored);
 
 export default sponsoringRoutes;
