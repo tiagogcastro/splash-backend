753b352512ffdd2ec2747e5d2d6aea4b39772a31
Tiaguin061 <tiaguin180@gmail.com>
2021-05-29 20:33:33 -0300

    feat: Merged router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -8,10 +8,7 @@ const router = Router();
 
 router.use(express.json());
 router.use('/users', usersRoutes);
-<<<<<<< HEAD
 router.use('/sms', smsRoutes);
-=======
 router.use('/sponsor', sponsoringRoutes);
->>>>>>> fe767e68cd469e01192353959a4abeb317217ec2
 
 export default router;
