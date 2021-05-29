bdcce17eefef099e423640438eab34dc13ffc7cd
Tiaguin061 <tiaguin180@gmail.com>
2021-05-29 20:29:36 -0300

    feat: Added new columns to user

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -6,22 +6,42 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+enum Permissions {
+  user = 0,
+  store,
+}
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  name: string;
+  name?: string;
 
   @Column()
   username: string;
 
   @Column()
-  email: string;
+  email?: string;
 
   @Column()
-  password: string;
+  password?: string;
+
+  @Column()
+  sponsoring: number;
+
+  @Column()
+  sponsored: number;
+
+  @Column('varchar')
+  phoneNumber?: string;
+
+  @Column()
+  money: number;
+
+  @Column('int')
+  permissions: Permissions;
 
   @CreateDateColumn()
   created_at: Date;
