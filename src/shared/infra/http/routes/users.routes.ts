e83b65536c3bded3e912cded9bf71b1945a6217d
Tiaguin061 <tiaguin180@gmail.com>
2021-05-29 20:35:16 -0300

    styles: Removed coments on code

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -5,27 +5,27 @@ import CreateUsersService from '@modules/users/services/CreateUsersService';
 const usersRoutes = Router();
 
 usersRoutes.post('/', async (req, res) => {
-  // try {
-  //   const { name, username, email, password } = await req.body;
-  //   const userService = new CreateUsersService();
-  //   const user = await userService.execute({
-  //     name,
-  //     username,
-  //     email,
-  //     password,
-  //   });
-  //   const userWithoutPassword = {
-  //     id: user.id,
-  //     name: user.name,
-  //     username: user.username,
-  //     email: user.email,
-  //     created_at: user.created_at,
-  //     updated_at: user.updated_at,
-  //   };
-  //   res.json(userWithoutPassword);
-  // } catch (err) {
-  //   res.json({ error: err.message });
-  // }
+  try {
+    const { name, username, email, password } = await req.body;
+    const userService = new CreateUsersService();
+    const user = await userService.execute({
+      name,
+      username,
+      email,
+      password,
+    });
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+    res.json(userWithoutPassword);
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 });
 
 export default usersRoutes;
