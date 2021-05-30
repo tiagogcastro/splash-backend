47a88557cb2c286c71c4390d9011ae437c5b7a53
Tiaguin061 <tiaguin180@gmail.com>
2021-05-30 17:12:39 -0300

    feat: Added use profile router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import smsRouter from '@modules/users/infra/http/routes/sms.routes';
 import sponsorsRouter from '@modules/users/infra/http/routes/sponsors.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import userProfileRuter from '@modules/users/infra/http/routes/profileUsers.routes';
 import sponsorshipsRouter from '@modules/sponsorships/infra/http/routes/sponsorships.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/profile', userProfileRuter);
 router.use('/sms', smsRouter);
 router.use('/sponsors', sponsorsRouter);
 router.use('/sponsorships', sponsorshipsRouter);
