482522df9745cb90bb3ab388f3b6058f134b4640
Tiaguin061 <tiaguin180@gmail.com>
2021-05-30 17:11:38 -0300

    feat: Created router for delete and update profile

--- src/modules/users/infra/http/routes/profileUsers.routes.ts
+++ src/modules/users/infra/http/routes/profileUsers.routes.ts
@@ -5,5 +5,7 @@ const profileRoutes = Router();
 const profileUserController = new ProfileUserController();
 
 profileRoutes.get('/:username',profileUserController.show);
+profileRoutes.put('/',profileUserController.update);
+profileRoutes.delete('/',profileUserController.delete);
 
 export default profileRoutes;
