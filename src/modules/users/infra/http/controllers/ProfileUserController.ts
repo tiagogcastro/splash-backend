52485ecf86e2acb724519203a24b88676c862a60
Tiaguin061 <tiaguin180@gmail.com>
2021-05-30 15:27:59 -0300

    feat: Added method to deleting profile

--- src/modules/users/infra/http/controllers/ProfileUserController.ts
+++ src/modules/users/infra/http/controllers/ProfileUserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import ShowProfileUserService from '@modules/users/services/ShowProfileUserService';
+import DeleteProfileUser from '@modules/users/services/DeleteProfileUser';
 
 class ProfileUserController {
   async show(request: Request, response: Response): Promise<Response> {
@@ -11,6 +12,16 @@ class ProfileUserController {
     const user = await showProfileUser.execute(username);
 
     return response.json(user);
+  }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const deleteProfile = new DeleteProfileUser();
+
+    await deleteProfile.execute(user_id);
+
+    return response.status(204).send();
   }
 }
 
---
+++ src/modules/users/services/DeleteProfileUser.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { getRepository } from 'typeorm';
+import User from '../infra/typeorm/entities/User';
+
+class DeleteProfileUser {
+  async execute(user_id: string): Promise<void> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { user_id },
+    });
+
+    if(!user) {
+      throw new AppError('User not exist', 400);
+    }
+
+    await usersRepository.delete(user_id);    
+  }
+}
+
+export default DeleteProfileUser;
