276a775f8fba6cb69acddead1b950d442bc78831
FireShark688 <gugacalb@gmail.com>
2021-05-30 21:26:05 -0300

    refactor

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,10 +4,12 @@ import sponsorsRouter from '@modules/users/infra/http/routes/sponsors.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import userProfileRuter from '@modules/users/infra/http/routes/profileUsers.routes';
 import sponsorshipsRouter from '@modules/sponsorships/infra/http/routes/sponsorships.routes';
+import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/sessions', sessionsRoutes);
 router.use('/profile', userProfileRuter);
 router.use('/sms', smsRouter);
 router.use('/sponsors', sponsorsRouter);
