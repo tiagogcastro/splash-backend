a866627da1645b5146b4f23e2e4c9eaf41a76979
x14102004x <marcosproenca144@gmail.com>
2021-05-31 20:33:41 -0300

    fix: The attribute was renamed to balance_amont

--- src/modules/sponsorships/dtos/ISendSponsorshipServiceDTO.ts
+++ src/modules/sponsorships/dtos/ISendSponsorshipServiceDTO.ts
@@ -2,5 +2,5 @@ export default interface ISendSponsorshipServiceDTO {
   user_recipient_id: string;
   sponsor_id: string;
   allow_withdrawal?: boolean;
-  your_sponsor_balance: number;
+  balance_amount: number;
 }
