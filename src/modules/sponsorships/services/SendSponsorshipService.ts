9c3c40d478c6edeabeaa74aaac6366e4de91eeca
x14102004x <marcosproenca144@gmail.com>
2021-05-31 21:26:52 -0300

    refactor: The variables became more readable

--- src/modules/sponsorships/services/SendSponsorshipService.ts
+++ src/modules/sponsorships/services/SendSponsorshipService.ts
@@ -5,6 +5,10 @@ import ISendSponsorshipServiceDTO from '../dtos/ISendSponsorshipServiceDTO';
 import Sponsor from '../infra/typeorm/entities/Sponsorship';
 import ISponsorshipsRepository from '../repositories/ISponsorshipsRepository';
 
+// Toda parte de enviar patriocion:
+// Quando clicar no botão "patrocinar" no perfil do usuario, chamará este service
+// Quando clicar em "patrocinar" na tela que lista todos os que eu já patrocíno, chamará este service
+
 export default class SendSponsorshipService {
   constructor(
     private usersRepository: IUsersRepository,
@@ -14,38 +18,59 @@ export default class SendSponsorshipService {
 
   async execute({
     user_recipient_id,
-    your_sponsor_balance,
-    allow_withdrawal = false,
     sponsor_id,
+    balance_amount,
+    allow_withdrawal = false,
   }: ISendSponsorshipServiceDTO): Promise<Sponsor> {
     let withdrawal_balance_available = true;
-
     const user = await this.usersRepository.findById(user_recipient_id);
     const sponsor = await this.usersRepository.findById(sponsor_id);
 
     if (!user) throw new AppError('The user does not exist', 401);
     if (!sponsor) throw new AppError('The sponsor does not exist', 401);
 
-    if (!(user.permissions === 1) && allow_withdrawal)
+    if (balance_amount <= 1 || balance_amount >= 500) {
+      throw new AppError(
+        'You cannot report a balance of less than 1 or greater than 500',
+        401,
+      );
+    }
+    if (!(sponsor.permissions === 'shop') && allow_withdrawal)
       throw new AppError('You are not allowed to access here', 401);
 
-    const message = {
-      name: user.username,
-      subject: `você recebeu R$${your_sponsor_balance} de ${sponsor.username}`,
-    };
-    await this.notificationsRepository.create({
-      recipient_id: user_recipient_id,
-      content: JSON.stringify(message),
-    });
+    let subject = `você enviou R$${balance_amount} para ${user.username}`;
 
-    if (user.permissions === 1) {
+    if (sponsor.permissions === 'shop') {
+      subject = `você pagou R$${balance_amount} para ${user.username}`;
       withdrawal_balance_available = allow_withdrawal;
     }
+
+    const messageFromRecipient = {
+      name: user.username,
+      subject: `você recebeu R$${balance_amount} de ${sponsor.username}`,
+    };
+
+    const messageFromSender = {
+      name: sponsor.username,
+      subject,
+    };
+
+    await this.notificationsRepository.create({
+      recipient_id: sponsor_id,
+      sender_id: sponsor_id,
+      content: JSON.stringify(messageFromSender),
+    });
+
+    await this.notificationsRepository.create({
+      recipient_id: user_recipient_id,
+      sender_id: sponsor_id,
+      content: JSON.stringify(messageFromRecipient),
+    });
 
     const sponsorship = await this.sponsorshipsRepository.create({
       user_recipient_id,
       sponsor_id,
-      your_sponsor_balance,
+      your_sponsor_balance: balance_amount,
       withdrawal_balance_available,
     });
 
