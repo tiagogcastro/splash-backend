a79721c1efc5b6f93bf3a75b4bab9cfea84886ab
x14102004x <marcosproenca144@gmail.com>
2021-05-31 21:18:18 -0300

    refactor: Now the logged in user is required to proceed

--- src/modules/sponsorships/infra/http/routes/sponsorships.routes.ts
+++ src/modules/sponsorships/infra/http/routes/sponsorships.routes.ts
@@ -1,9 +1,12 @@
+import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import { Router } from 'express';
 import SponsorshipsController from '../controllers/SponsorshipsController';
 
 const sponsorshipsRouter = Router();
 
 const sponsorshipsController = new SponsorshipsController();
+
+sponsorshipsRouter.use(ensureAuthenticated);
 
 sponsorshipsRouter.post('/', sponsorshipsController.create);
 
