6c96e0ed4a0cdde666c3bf54575da893e7df08e8
x14102004x <marcosproenca144@gmail.com>
2021-06-01 22:27:18 -0300

    feat: The list and search dtos were created and implemented

---
+++ src/modules/notifications/dtos/IFindAllSponsorshipNotificationsDTO.ts
@@ -0,0 +1,4 @@
+export default interface IFindAllSponsorshipNotificationsDTO {
+  recipient_id: string;
+  sender_id: string;
+}
---
+++ src/modules/sponsorships/dtos/ISearchListSponsoredFromUserDTO.ts
@@ -0,0 +1,4 @@
+export default interface ISearchListSponsoredFromUserDTO {
+  username: string;
+  sponsor_id: string;
+}
