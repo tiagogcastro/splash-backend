c1b13a2ebbb19161f316855ca653b3c849da6fc2
x14102004x <marcosproenca144@gmail.com>
2021-06-02 21:42:34 -0300

    feat: The logic behind the avatar update

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -14,20 +14,23 @@ class UpdateUserAvatarService {
     private storageProvider: IStorageProvider,
   ) {}
 
-  /**
-   * execute
-   */
-  public async execute({ user_id, avatarFileName }: RequestDTO): Promise<void> {
-    // const user = await this.usersRepository.findById(user_id);
-    // if (!user)
-    //   throw new AppError('Only autheticated users can change avatar.', 401);
-    // if (user.avatar) {
-    //   await this.storageProvider.deleteFile(user.avatar);
-    // }
-    // const filename = await this.storageProvider.saveFile(avatarFileName);
-    // user.avatar = filename;
-    // await this.usersRepository.save(user);
-    // return user;
+  public async execute({ user_id, avatarFileName }: RequestDTO): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user)
+      throw new AppError('Only autheticated users can change avatar', 401);
+
+    if (user.avatar) {
+      await this.storageProvider.deleteFile(user.avatar);
+    }
+
+    user.avatar = avatarFileName;
+
+    await this.storageProvider.saveFile(avatarFileName);
+
+    await this.usersRepository.save(user);
+
+    return user;
   }
 }
 export default UpdateUserAvatarService;
