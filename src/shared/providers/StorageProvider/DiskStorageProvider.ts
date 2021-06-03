5c8b56ab895a442297d79983cbf1abf61597cb2e
x14102004x <marcosproenca144@gmail.com>
2021-06-02 21:44:31 -0300

    feat: The save and delete methods completed successfully

--- src/shared/providers/StorageProvider/DiskStorageProvider.ts
+++ src/shared/providers/StorageProvider/DiskStorageProvider.ts
@@ -1,11 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '@config/upload';
 import IStorageProvider from './models/IStorageProvider';
 
-export default class DiskStorageProvider implements IStorageProvider {
-  async saveFile(file: string): Promise<string> {
-    throw new Error('Method not implemented.');
+class DiskStorageProvider implements IStorageProvider {
+  async deleteFile(file: string): Promise<void> {
+    const filePath = path.resolve(uploadConfig.uploadsFolder, file);
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+    await fs.promises.unlink(filePath);
   }
 
-  async deleteFile(file: string): Promise<void> {
-    throw new Error('Method not implemented.');
+  async saveFile(file: string): Promise<string> {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.tmpFolder, file),
+      path.resolve(uploadConfig.uploadsFolder, file),
+    );
+
+    return file;
   }
 }
+export default DiskStorageProvider;
