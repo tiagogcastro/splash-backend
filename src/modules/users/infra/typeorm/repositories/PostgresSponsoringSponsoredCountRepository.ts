698430b2b6d17cb8adae989657f76856a0eea05e
Tiaguin061 <tiaguin180@gmail.com>
2021-06-04 14:42:51 -0300

    feat: Added repository to sponsoring and sponsored count

---
+++ src/modules/users/infra/typeorm/repositories/PostgresSponsoringSponsoredCountRepository.ts
@@ -0,0 +1,24 @@
+import IUpdateSponsoringSponsoredCountDTO from '@modules/users/dtos/IUpdateSponsoringSponsoredCountDTO';
+import ISponsoringSponsoredCountRepository from '@modules/users/repositories/ISponsoringSponsoredCountRepository';
+import { getRepository, Repository } from 'typeorm';
+import SponsoringSponsoredCount from '../entities/SponsoringSponsoredCount';
+
+export default class PostgresSponsoringSponsoredCountRepository
+  implements ISponsoringSponsoredCountRepository
+{
+  private ormRepository: Repository<SponsoringSponsoredCount>;
+
+  constructor() {
+    this.ormRepository = getRepository(SponsoringSponsoredCount);
+  }
+
+  async updateCount(user_id: string, data: IUpdateSponsoringSponsoredCountDTO): Promise<SponsoringSponsoredCount | undefined> {
+    const count = await this.ormRepository.update(user_id, data);
+
+    if(count.affected === 1) {
+      const countUpdated = await this.ormRepository.findOne(user_id);
+
+      return countUpdated;
+    }
+  }
+}
