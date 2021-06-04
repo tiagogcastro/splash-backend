afbf96cf219e94245ff7214838d92a02b49b11d0
x14102004x <marcosproenca144@gmail.com>
2021-06-04 15:32:40 -0300

    feat: Two listing routes were created

--- src/modules/sponsorships/infra/http/routes/sponsorships.routes.ts
+++ src/modules/sponsorships/infra/http/routes/sponsorships.routes.ts
@@ -1,16 +1,19 @@
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import { Router } from 'express';
-import SponsorshipsController from '../controllers/SponsorshipsController';
 import SponsoredController from '../controllers/SponsoredController';
+import SponsorshipsController from '../controllers/SponsorshipsController';
+import SponsoredMeController from '../controllers/SponsoredMeController';
 
 const sponsorshipsRouter = Router();
 
 const sponsorshipsController = new SponsorshipsController();
 const sponsoredController = new SponsoredController();
+const sponsoredMeController = new SponsoredMeController();
 
 sponsorshipsRouter.use(ensureAuthenticated);
 
-sponsorshipsRouter.get('/sponsored/me', sponsoredController.index);
+sponsorshipsRouter.get('/sponsored/me', sponsoredMeController.index);
+sponsorshipsRouter.get('/sponsored', sponsoredController.index);
 sponsorshipsRouter.post('/', sponsorshipsController.create);
 
 export default sponsorshipsRouter;
