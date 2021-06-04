9643b9fcbf994c81f0ebdd5baac412b94aad1304
Tiaguin061 <tiaguin180@gmail.com>
2021-06-04 14:41:45 -0300

    feat: Created sponsoring_sponsored entity

---
+++ src/modules/users/infra/typeorm/entities/SponsoringSponsoredCount.ts
@@ -0,0 +1,42 @@
+import {
+  ObjectID,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import User from './User';
+
+@Entity('sponsorsing_sponsored')
+class SponsoringSponsoredCount {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  user_id: string;
+  
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  sponsor_count: number;
+
+  sponsored_count: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default SponsoringSponsoredCount;
