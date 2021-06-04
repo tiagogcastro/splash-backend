8dfdb16deb923d721d8ff24697afb0af5da830a8
x14102004x <marcosproenca144@gmail.com>
2021-06-04 15:33:37 -0300

    feat: Four methods were added respecting SOLID

--- src/modules/sponsorships/infra/typeorm/repositories/PostgresSponsorshipsRepository.ts
+++ src/modules/sponsorships/infra/typeorm/repositories/PostgresSponsorshipsRepository.ts
@@ -1,4 +1,6 @@
 import ICreateSponsorshipDTO from '@modules/sponsorships/dtos/ICreateSponsorshipDTO';
+import IFindSponsorshipDTO from '@modules/sponsorships/dtos/IFindSponsorshipDTO';
+import IFindSponsorshipUnavailableDTO from '@modules/sponsorships/dtos/IFindSponsorshipUnavailableDTO';
 import ISponsorshipsRepository from '@modules/sponsorships/repositories/ISponsorshipsRepository';
 import { getRepository, Repository } from 'typeorm';
 import Sponsorship from '../entities/Sponsorship';
@@ -10,6 +12,50 @@ export default class PostgresSponsorshipsRepository
 
   constructor() {
     this.ormRepository = getRepository(Sponsorship);
+  }
+
+  async findAllSponsorshipsFromUser(
+    sponsored_user_id: string,
+  ): Promise<Sponsorship[]> {
+    const sponsorships = await this.ormRepository.find({
+      where: {
+        sponsored_user_id,
+      },
+      relations: ['sponsor'],
+    });
+    return sponsorships;
+  }
+
+  async findSponsorshipUnavailable({
+    sponsor_user_id,
+    sponsored_user_id,
+  }: IFindSponsorshipUnavailableDTO): Promise<Sponsorship | undefined> {
+    const sponsorship = await this.ormRepository.findOne({
+      where: {
+        sponsor_user_id,
+        sponsored_user_id,
+        allow_withdrawal: false,
+        redeemed: false,
+      },
+    });
+    return sponsorship;
+  }
+
+  async save(sponsorship: Sponsorship): Promise<Sponsorship> {
+    return this.ormRepository.save(sponsorship);
+  }
+
+  async findSponsorship({
+    sponsor_user_id,
+    sponsored_user_id,
+  }: IFindSponsorshipDTO): Promise<Sponsorship | undefined> {
+    const sponsorship = await this.ormRepository.findOne({
+      where: {
+        sponsor_user_id,
+        sponsored_user_id,
+      },
+    });
+    return sponsorship;
   }
 
   async findAllSponsoredFromUser(
