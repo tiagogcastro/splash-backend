6a0c42e88923bb44c84121295a0b65aa6949f5c9
Tiaguin061 <tiaguin180@gmail.com>
2021-06-04 14:35:08 -0300

    feat: Finalized create user from email

--- src/modules/users/services/CreateUsersByEmailService.ts
+++ src/modules/users/services/CreateUsersByEmailService.ts
@@ -1,10 +1,13 @@
 /* eslint-disable no-param-reassign */
 // eslint-disable-next-line import/no-unresolved
 import jwtConfig from '@config/auth';
+import ISponsorshipsRepository from '@modules/sponsorships/repositories/ISponsorshipsRepository';
 import AppError from '@shared/errors/AppError';
 import { hash } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '../infra/typeorm/entities/User';
+import ISponsoringRepository from '../repositories/ISponsoringRepository';
+import ISponsoringSponsoredCountRepository from '../repositories/ISponsoringSponsoredCountRepository';
 import IUserBalanceRepository from '../repositories/IUserBalanceRepository';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -13,6 +16,8 @@ interface Request {
   email: string;
   username?: string;
   password: string;
+  sponsorship_code: string;
+  terms: boolean;
 }
 
 interface Response {
@@ -24,6 +29,9 @@ export default class CreateUsersService {
   constructor(
     private usersRepository: IUsersRepository,
     private userBalanceRepository: IUserBalanceRepository,
+    private sponsorships: ISponsorshipsRepository,
+    private sponsoring: ISponsoringRepository,
+    private sponsoringSponsoredCount: ISponsoringSponsoredCountRepository
   ) {}
 
   public async execute({
@@ -31,12 +39,12 @@ export default class CreateUsersService {
     username,
     email,
     password,
+    sponsorship_code,
+    terms
   }: Request): Promise<Response> {
     const checkUserEmailExist = await this.usersRepository.findByEmail(email);
-
-    const checkUserUsernameExist = await this.usersRepository.findByUsername(
-      username,
-    );
+    const sponsorshipExist = await this.sponsorships.findBySponsorshipCode(sponsorship_code);
+    const checkUserUsernameExist = await this.usersRepository.findByUsername(username);
 
     if (checkUserEmailExist) {
       throw new AppError('E-mail address already used.');
@@ -46,11 +54,23 @@ export default class CreateUsersService {
       throw new AppError('Username address already used.', 400);
     }
 
+    if(!sponsorshipExist || sponsorshipExist.sponsorship_code !== sponsorship_code || sponsorshipExist.redeemed == true) {
+      throw new AppError('Código de patrocínio inválido ou já usado.', 400);
+    }
+
+    if(!terms) {
+      throw new AppError('Você não aceitou os termos.', 400);
+    }
+
     if (!username) {
       const randomUsername = `username.${Math.random()
         .toFixed(4)
         .replace('.', '')}${new Date().getTime()}`;
       username = randomUsername;
+    }
+
+    if(username.length > 24) {
+      throw new AppError('Username só pode ter no máximo 24 caracteres', 400);
     }
 
     username = username.replace(/\s/g, '');
@@ -59,17 +79,49 @@ export default class CreateUsersService {
       name = `name.${Math.random().toFixed(4).replace('.', '')}`;
     }
 
+    if(name.length > 30) {
+      throw new AppError('O nome só pode ter no máximo 30 caracteres', 400);
+    }
+
     const hashedPassword = await hash(password, 8);
 
     const user = await this.usersRepository.create({
-      name: name || `name.lavimco-${Math.random().toFixed(4)}`,
+      name,
       username,
       email,
       password: hashedPassword,
     });
 
+    // Deixa o patrocinio resgatado e indisponivel
+    await this.sponsorships.updateSponsorship(sponsorshipExist.sponsor_user_id, {
+      sponsored_user_id: user.id,
+      redeemed: true,
+    });
+
+    // Cria o saldo e adiciona la
     await this.userBalanceRepository.create({
       user_id: user.id,
+      total_balance: sponsorshipExist.amount,
+    });
+
+    // remove do saldo da loja o valor informado no patrocinio
+    await this.userBalanceRepository.update(sponsorshipExist.sponsor_user_id, {
+      total_balance: - sponsorshipExist.amount,
+    });
+
+    // A loja passa a patrocinar o usuário
+    await this.sponsoring.create({
+      sponsoring_userId: sponsorshipExist.sponsor_user_id,
+      sponsored_userId: user.id,
+    });
+
+    // Loja fica com +1 patrocinado e o usuário fica com +1 patrocinando ele
+    await this.sponsoringSponsoredCount.updateCount(sponsorshipExist.sponsor_user_id, {
+      sponsor_count: + 1,
+    });
+
+    await this.sponsoringSponsoredCount.updateCount(user.id, {
+      sponsored_count: + 1,
     });
 
     const { secret, expiresIn } = jwtConfig.jwt;
