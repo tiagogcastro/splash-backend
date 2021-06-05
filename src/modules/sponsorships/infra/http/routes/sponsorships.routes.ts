257bf7f495cadd2d516a186e66bd69a3fba2cbe9
x14102004x <marcosproenca144@gmail.com>
2021-06-05 19:27:16 -0300

    refactor: The ensurePayment middleware was added

--- src/modules/sponsorships/infra/http/routes/sponsorships.routes.ts
+++ src/modules/sponsorships/infra/http/routes/sponsorships.routes.ts
@@ -1,6 +1,7 @@
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import ensurePayment from '../middlewares/ensurePayment';
 import SponsoredController from '../controllers/SponsoredController';
 import SponsorshipsController from '../controllers/SponsorshipsController';
 import SponsoredMeController from '../controllers/SponsoredMeController';
@@ -17,6 +18,7 @@ sponsorshipsRouter.get('/sponsored/me', sponsoredMeController.index);
 sponsorshipsRouter.get('/sponsored', sponsoredController.index);
 sponsorshipsRouter.post(
   '/',
+  ensurePayment,
   celebrate({
     [Segments.BODY]: {
       user_recipient_id: Joi.string().uuid().required(),
