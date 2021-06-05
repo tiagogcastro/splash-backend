d2564fed161d60f1d1ebe15fed592f0203f8f59c
FireShark688 <gugacalb@gmail.com>
2021-06-05 16:50:37 -0300

    .

--- src/modules/users/infra/http/controllers/UsersEmailController.ts
+++ src/modules/users/infra/http/controllers/UsersEmailController.ts
@@ -1,9 +1,9 @@
 import { classToClass } from 'class-transformer';
 import CreateUsersService from '@modules/users/services/CreateUsersByEmailService';
 import { Request, Response } from 'express';
+import PostgresSponsorshipsRepository from '@modules/sponsorships/infra/typeorm/repositories/PostgresSponsorshipsRepository';
 import PostgresUsersRepository from '../../typeorm/repositories/PostgresUsersRepository';
 import PostgresUserBalanceRepository from '../../typeorm/repositories/PostgresUserBalanceRepository';
-import PostgresSponsorshipsRepository from '@modules/sponsorships/infra/typeorm/repositories/PostgresSponsorshipsRepository';
 import PostgresSponsoringRepository from '../../typeorm/repositories/PostgresSponsoringRepository';
 import PostgresSponsoringSponsoredCountRepository from '../../typeorm/repositories/PostgresSponsoringSponsoredCountRepository';
 
@@ -11,18 +11,20 @@ const postgresUsersRepository = new PostgresUsersRepository();
 const postgresUserBalanceRepository = new PostgresUserBalanceRepository();
 const postgresSponsorshipsRepository = new PostgresSponsorshipsRepository();
 const postgresSponsoringRepository = new PostgresSponsoringRepository();
-const postgresSponsoringSponsoredCountRepository = new PostgresSponsoringSponsoredCountRepository();
+const postgresSponsoringSponsoredCountRepository =
+  new PostgresSponsoringSponsoredCountRepository();
 
 class UsersEmailController {
   async create(request: Request, response: Response): Promise<Response> {
-    const { name, username, email, password, sponsorship_code, terms } = await request.body;
+    const { name, username, email, password, sponsorship_code, terms } =
+      await request.body;
 
     const createUser = new CreateUsersService(
       postgresUsersRepository,
       postgresUserBalanceRepository,
       postgresSponsorshipsRepository,
       postgresSponsoringRepository,
-      postgresSponsoringSponsoredCountRepository
+      postgresSponsoringSponsoredCountRepository,
     );
 
     const { user, token } = await createUser.execute({
@@ -31,7 +33,7 @@ class UsersEmailController {
       email,
       password,
       sponsorship_code,
-      terms
+      terms,
     });
 
     return response.json({
