869a5c9a90b74cbc35e443e93736108b7d717795
Tiaguin061 <tiaguin180@gmail.com>
2021-06-06 11:28:38 -0300

    fix: Removed two fields that no longer exist

--- src/modules/users/infra/typeorm/entities/UserBalance.ts
+++ src/modules/users/infra/typeorm/entities/UserBalance.ts
@@ -27,16 +27,6 @@ class UserBalance {
   @Column()
   total_balance: number;
 
-  @Column({
-    default: 0,
-  })
-  available_balance_amount: number;
-
-  @Column({
-    default: 0,
-  })
-  unvailable_balance_amount: number;
-
   @CreateDateColumn()
   created_at: Date;
 
