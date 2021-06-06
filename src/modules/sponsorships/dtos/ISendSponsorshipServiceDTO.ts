53c67d5bf72d489d7f8cc8d11b62f7968fc244f4
x14102004x <marcosproenca144@gmail.com>
2021-06-05 22:07:01 -0300

    refactor Send sponsorship DTO was sponsorship code added

--- src/modules/sponsorships/dtos/ISendSponsorshipServiceDTO.ts
+++ src/modules/sponsorships/dtos/ISendSponsorshipServiceDTO.ts
@@ -1,6 +1,7 @@
 export default interface ISendSponsorshipServiceDTO {
   user_recipient_id: string;
   sponsor_user_id: string;
+  sponsorship_code: boolean;
   amount: number;
   allow_withdrawal_balance: boolean;
 }
