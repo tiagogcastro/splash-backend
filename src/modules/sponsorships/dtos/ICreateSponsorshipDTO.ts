9ffaeb0886a134aec5ed4fdd46ebcc29e71ad54a
x14102004x <marcosproenca144@gmail.com>
2021-06-05 22:04:56 -0300

    refactor: The sponsorship code attribute was added

--- src/modules/sponsorships/dtos/ICreateSponsorshipDTO.ts
+++ src/modules/sponsorships/dtos/ICreateSponsorshipDTO.ts
@@ -2,5 +2,6 @@ export default interface ICreateSponsorshipDTO {
   sponsored_user_id: string;
   sponsor_user_id: string;
   amount: number;
+  sponsorship_code?: string;
   allow_withdrawal: boolean;
 }
