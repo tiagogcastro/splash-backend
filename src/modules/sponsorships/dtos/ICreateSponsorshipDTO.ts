6e9a428fa3e7ee9f1e675e50e8df937333d886c3
x14102004x <marcosproenca144@gmail.com>
2021-06-06 21:30:43 -0300

    feat: The recipient is now not required

--- src/modules/sponsorships/dtos/ICreateSponsorshipDTO.ts
+++ src/modules/sponsorships/dtos/ICreateSponsorshipDTO.ts
@@ -1,5 +1,5 @@
 export default interface ICreateSponsorshipDTO {
-  sponsored_user_id: string;
+  sponsored_user_id?: string;
   sponsor_user_id: string;
   amount: number;
   sponsorship_code?: string;
