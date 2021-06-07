919a0759ed51d9e53fe0880fd5f174fe4fefb789
x14102004x <marcosproenca144@gmail.com>
2021-06-06 21:35:57 -0300

    feat: The entity sponsor balance was created

---
+++ src/modules/users/infra/typeorm/entities/SponsorBalance.ts
@@ -0,0 +1,51 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import User from './User';
+
+@Entity('sponsor_balance')
+class SponsorBalance {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  available_for_withdrawal: number;
+
+  @Column()
+  balance_amount: number;
+
+  @Column()
+  sponsored_user_id: string;
+
+  @Column()
+  sponsor_shop_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'sponsored_user_id' })
+  sponsored: User;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'sponsor_shop_id' })
+  sponsor: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default SponsorBalance;
