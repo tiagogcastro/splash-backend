71121d6da179187e3a8d71be3fb5b8563b926a7a
Tiaguin061 <tiaguin180@gmail.com>
2021-06-10 19:46:02 -0300

    feat: Added ensure authenticated

--- src/modules/users/infra/http/routes/sponsors.routes.ts
+++ src/modules/users/infra/http/routes/sponsors.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import SponsorController from '../controllers/SponsorUserController';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
 import SponsoringUserController from '../controllers/SponsoringUserController';
 import SponsoredUserController from '../controllers/SponsoredUserController';
 
 const sponsorsRoutes = Router();
+
+sponsorsRoutes.use(ensureAuthenticated);
 
 const sponsorUserController = new SponsorController();
 const sponsoringUserController = new SponsoringUserController();
