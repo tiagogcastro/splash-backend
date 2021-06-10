14d5e55a8db7cd1fdd2f98999a65ddc349a99e67
Tiaguin061 <tiaguin180@gmail.com>
2021-06-10 19:42:24 -0300

    feat: Added roles as optional

--- src/modules/users/dtos/ICreateUserDTO.ts
+++ src/modules/users/dtos/ICreateUserDTO.ts
@@ -1,7 +1,10 @@
+type Roles = 'admin' | 'user' | 'shop';
+
 export default interface ICreateUserDTO {
   email?: string;
   name?: string;
   password?: string;
   username: string;
   phone_number?: string;
+  roles?: Roles;
 }
