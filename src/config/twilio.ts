f3c4e662a99fc93cd7334b947e7b9b51a828e837
Tiago Gonçalves de Castro <tiaguin180@gmail.com>
2021-06-29 19:39:03 -0300

    Update twilio.ts

--- src/config/twilio.ts
+++ src/config/twilio.ts
@@ -1,7 +1,7 @@
 export default {
   twilio: {
-    accountSid: process.env.TWILLO_ACCOUNT_SID || '*',
-    authToken: process.env.TWILLO_AUTH_TOKEN || '*',
-    servicesSid: process.env.TWILLO_SERVICES_SID || '*',
+    accountSid: '',
+    authToken: '',
+    servicesSid: '',
   },
 };
